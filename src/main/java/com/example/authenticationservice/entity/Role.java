abd56d2a5f8cf27dcc7a1c0f64d9f94b7c129a5c
taya reut <tasyareut@gmail.com>
2023-12-02 14:58:12 +0100

    Login service for user - registration - authentication - authorization - JWT implementation - logout functionality

--- src/main/java/com/example/authenticationservice/entity/Role.java
+++ src/main/java/com/example/authenticationservice/entity/Role.java
@@ -1,13 +1,5 @@
 package com.example.authenticationservice.entity;
 
-import lombok.Getter;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 public enum Role {
 
     USER,
